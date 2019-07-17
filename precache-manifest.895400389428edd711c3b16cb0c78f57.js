0038e88668215903cb59955500f40064103c8466
Khyati Goyal <17uec065@lnmiit.ac.in>
2019-07-17 23:17:59 +0530

    Updates

---
+++ precache-manifest.895400389428edd711c3b16cb0c78f57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8172fec8a81f89a0736890b79d70e320",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "736bb82d1b347dbc8973",
+    "url": "/seasons/static/css/main.c5c5e061.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "736bb82d1b347dbc8973",
+    "url": "/seasons/static/js/main.2d427f5e.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
